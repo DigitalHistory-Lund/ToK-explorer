a3002c4778c1634ec7e3bb2611ea58381c550f33
Mathias Johansson <MathiasJoha@gmail.com>
2026-01-21 17:07:18 +0100

    Split notebeook

--- build-quarto.py
+++ build-quarto.py
@@ -3,40 +3,46 @@ from pathlib import Path
 
 here = Path(__file__).parent
 
-index = here / "index.qmd"
-subprocess.run(
-    [
-        "quarto",
-        "convert",
-        str(here / "ToK_exploration.ipynb"),
-        "--output",
-        str(index),
-    ]
-)
+for notebook in here.glob("*.ipynb"):
+    if notebook.name == "ToK_exploration.ipynb":
+        out_file = here / "index.qmd"
+    else:
+        out_file = here / f"{notebook.stem}.qmd"
 
-with open(index, "r", encoding="utf8") as f:
-    lines = [
-        line
-        for line in f.readlines()
-        if not line.startswith("#| colab:") and not line.startswith("#| executionInfo:")
-    ]
+    subprocess.run(
+        [
+            "quarto",
+            "convert",
+            str(here / "ToK_exploration.ipynb"),
+            "--output",
+            str(out_file),
+        ]
+    )
 
-with open(index, "w", encoding="utf8") as f:
-    f.writelines(lines)
+    with open(out_file, "r", encoding="utf8") as f:
+        lines = [
+            line
+            for line in f.readlines()
+            if not line.startswith("#| colab:")
+            and not line.startswith("#| executionInfo:")
+        ]
 
-with open(index, "r", encoding="utf8") as f:
-    content = f.read()
+    with open(out_file, "w", encoding="utf8") as f:
+        f.writelines(lines)
 
-content = content.replace(
-    "jupyter: tok\n",
-    """jupyter: tok
-execute:
-  echo: false
-author:
-    - Mathias Johansson
-    - Ulrika Holgersson
-""",
-)
+    with open(out_file, "r", encoding="utf8") as f:
+        content = f.read()
 
-with open(index, "w", encoding="utf8") as f:
-    f.write(content)
+    content = content.replace(
+        "jupyter: tok\n",
+        """jupyter: tok
+    execute:
+    echo: false
+    author:
+        - Mathias Johansson
+        - Ulrika Holgersson
+    """,
+    )
+
+    with open(out_file, "w", encoding="utf8") as f:
+        f.write(content)
